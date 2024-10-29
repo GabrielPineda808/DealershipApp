39d8d4c815f630fb9dfc4dc36961eb6ef5056c00
Gabe <gabepineda6@gmail.com>
2024-10-29 08:18:01 -0700

    Created a file reader and buffered reader that will create new vehicle ojbects based on the csv of vehicle info. It will also create a new dealship and add the vehicles to the dealership

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,12 +1,34 @@
 package com.pluralsight;
 
+import java.io.*;
+
 public class DealershipFileManager {
 
     public static Dealership getDealership(){
-        return null;
+
+        Dealership dealership;
+
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader("inventory.csv"));
+
+            String line;
+
+            String[] dealer = br.readLine().split("\\|");;
+
+            dealership = new Dealership(dealer[0],dealer[1],dealer[2]);
+
+            while ((line = br.readLine())!= null){
+                String[] data = line.split("\\|");
+                Vehicle vehicle = new Vehicle(Integer.parseInt(data[0]),Integer.parseInt(data[1]),data[2],data[3],data[4],data[5],Integer.parseInt(data[6]), Double.parseDouble(data[7]));
+                dealership.addVehicle(vehicle);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return  dealership;
     }
 
     public static void saveDealership(Dealership dealership){
-
     }
 }
