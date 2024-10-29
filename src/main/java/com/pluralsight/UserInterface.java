65fa2f806fe5e9988612a7832055a1e4e08c9841
Gabe <gabepineda6@gmail.com>
2024-10-29 07:37:58 -0700

    Created a User Interface class that will handles all calls and request plus prompt the user with text q

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,52 @@
 package com.pluralsight;
 
 public class UserInterface {
+    Dealership dealership;
+
+    public UserInterface() {
+
+    }
+
+    public static void display(){
+
+    }
+
+    public static void processGetByPriceRequest (){
+
+    }
+
+    public static void processGetByMakeModelRequest (){
+
+    }
+
+    public static void processGetByYearRequest (){
+
+    }
+
+    public static void processGetByColorRequest (){
+
+    }
+
+    public static void processGetByMileageRequest (){
+
+    }
+
+    public static void processGetByVehicleTypeRequest (){
+
+    }
+
+    public static void processGetAllVehiclesRequest (){
+
+    }
+
+    public static void processAddVehicleRequest (){
+
+    }
+
+    public static void processRemoveVehicleRequest(){
+
+    }
+
+
+
 }
